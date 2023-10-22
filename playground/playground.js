2b7d4f4d912f8909efbfb1a7d4cba2f658c59491
Eric Prud'hommeaux <eric+github@w3.org>
2023-10-22 12:39:09 +0200

    + multi-manifest

--- playground/playground.js
+++ playground/playground.js
@@ -2,6 +2,7 @@ const MANIFEST = {
   param: 'manifest',
   default: 'playground/manifests/manifest.yaml'
 };
+
 class Playground {
 
   constructor () {
@@ -21,14 +22,8 @@ class Playground {
       const body = await this.getBody(manifestURL, 'manifest');
       now = `parse manifest ${body}`;
       const manifest = Stuff.JsYaml.load(body);
-      $('#manifest').append(
-        manifest.map(item => $(`<li/>`).append(
-          $(`<button/>`)
-            .text(item.manifestLabel)
-            .on('click', evt => this.manifestClick(this, item))
-        ))
-      );
-      console.log(manifest)
+      this.paintManifest(manifest, $('body > .header'), this.headerManifestSelect.bind(this));
+      console.log(`Manifest <${manifestURL}>:`, manifest)
     } catch (e) {
       console.error(e);
       document.querySelector('body').classList.add('error');
@@ -36,12 +31,17 @@ class Playground {
     }
   }
 
-  async manifestClick (evt, manifestEntry) {
-    const manifestKeyToInput = {
-      data: '#data textarea',
-      dataFormat: '#data select',
-      sparqlQuery: '#query textarea',
-    }
+  paintManifest (manifest, from, action) {
+    from.find('.manifest').append(
+      manifest.map(item => $(`<li/>`).append(
+        $(`<button/>`)
+          .text(item.label)
+          .on('click', evt => action(evt, item))
+      ))
+    )
+  }
+
+  async genericManifestSelect (manifestEntry, manifestKeyToInput) {
     for (const [manifestKey, selector] of Object.entries(manifestKeyToInput)) {
       $(selector).empty();
       const derefMe = `${manifestKey}URL`;
@@ -58,43 +58,36 @@ class Playground {
         $(selector).val(manifestEntry[manifestKey]);
       }
     }
+  }
+
+  async headerManifestSelect (evt, manifestEntry) {
+    const manifestKeyToInput = {
+      data: '#data textarea',
+      dataFormat: '#data select',
+      sparqlQuery: '#query textarea',
+    }
+    this.genericManifestSelect(manifestEntry, manifestKeyToInput);
     $('#text').empty();
     if ('text' in manifestEntry)
       $('#text').get(0).innerHTML = manifestEntry.text;
     if ($('#query textarea')) {
       $('#query button')
         .prop('disabled', false)
-        .on('click', async evt => {
-          $('#results').empty();
-          const db = new N3.Store();
-          const parser = new N3.Parser({baseIRI: location.href})
-          db.addQuads(parser.parse($('#data textarea').val()));
-          const myEngine = new Comunica.QueryEngine();
-          const query = $('#query textarea').val();
-          const typedStream = await myEngine.queryBindings(query, {sources: [db]});
-          const typed = (await typedStream.toArray()).map(
-            b => Object.fromEntries(b.entries)
-          );
-          if (typed.length === 0) {
-            $('#results').text('no results');
-          } else {
-            $('#results').text(`${typed.length} results`);
-            const variables = Object.keys(typed[0]); // TODO: look for more elegant solution
-            const heading = $('<tr/>').append('<th/>').text('#');
-            heading.append(variables.map(v => $('<th/>').text(v)));
-            const table = $('<table/>').append($('<thead/>').append(heading));
-            $('#results').append(table);
-            for (const rowNo in typed) {
-              const row = typed[rowNo];
-              table.append(
-                $('<tr/>').append([ $('<td/>').text('' + rowNo) ].concat(
-                  variables.map(v => $('<td/>').text(row[v].value))
-                ) )
-              )
-            }
-          }
-          console.log(typed);
-        })
+        .on('click', this.executeQuery.bind(this))
+    }
+    if (manifestEntry.sparqlQueries) {
+      this.paintManifest(manifestEntry.sparqlQueries, $('#query > .header'), this.queryManifestSelect.bind(this));
+    }
+  }
+
+  async queryManifestSelect (evt, manifestEntry) {
+    this.genericManifestSelect(manifestEntry, {
+      sparqlQuery: '#query textarea',
+    });
+    if ($('#query textarea')) {
+      $('#query button')
+        .prop('disabled', false)
+        .on('click', this.executeQuery.bind(this))
     }
   }
 
@@ -104,6 +97,38 @@ class Playground {
     if (!resp.ok)
       throw Error(`Unable to fetch ${what} at <${url}>:\n${body}`);
     return body;
+  }
+
+  async executeQuery (evt) {
+    $('#results').empty();
+    const db = new N3.Store();
+    const parser = new N3.Parser({baseIRI: location.href})
+    db.addQuads(parser.parse($('#data textarea').val()));
+    const myEngine = new Comunica.QueryEngine();
+    const query = $('#query textarea').val();
+    const typedStream = await myEngine.queryBindings(query, {sources: [db]});
+    const typed = (await typedStream.toArray()).map(
+      b => Object.fromEntries(b.entries)
+    );
+    if (typed.length === 0) {
+      $('#results').text('no results');
+    } else {
+      $('#results').text(`${typed.length} results`);
+      const variables = Object.keys(typed[0]); // TODO: look for more elegant solution
+      const heading = $('<tr/>').append('<th/>').text('#');
+      heading.append(variables.map(v => $('<th/>').text(v)));
+      const table = $('<table/>').append($('<thead/>').append(heading));
+      $('#results').append(table);
+      for (const rowNo in typed) {
+        const row = typed[rowNo];
+        table.append(
+          $('<tr/>').append([ $('<td/>').text('' + rowNo) ].concat(
+            variables.map(v => $('<td/>').text(row[v].value))
+          ) )
+        )
+      }
+    }
+    console.log(typed);
   }
 }
 
