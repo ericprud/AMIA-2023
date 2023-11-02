15ee4154a35f5f63ecbcda039c6f2dff56ed3ae5
Eric Prud'hommeaux <eric+github@w3.org>
2023-11-02 20:59:20 +0100

    ~ RdfToJson += support for literals in lists

--- playground/rdfToJson.js
+++ playground/rdfToJson.js
@@ -13,35 +13,8 @@ let tStrs0 = [
   /*8*/ "<s9> <p9> 9 .",
 ];
 
-let turtle = `
-<s1>
-  <p1> ( # (_:n3-0, _:n3-4)
-    [ <p2>  ( # _:n3-1 <p2> (_:n3-2)
-      [ <p3> 3; <p4> 4 ] # _:n3-3 <p3> 3; <p4> 4
-    ) ]
-    [ <p5> 5 ] # _:n3-5 <p5> 5
-  ) ;
-  <p6> [ <p7> 7; <p8> 8 ] .
-<s9> <p9> 9 .
-`;
-let tStrs = [
-  /* 0*/ '_:n3-0 rdf:first _:n3-1 .', // ( [ <p2> @_:n3-2 ] [ <p5> 5 ] )
-  /* 1*/ '_:n3-2 rdf:first _:n3-3 .', // ( [ <p3> 3 ; <p4> 4 ] )
-  /* 2*/ '_:n3-3 <p3> "3"^^xsd:integer .',
-  /* 3*/ '_:n3-3 <p4> "4"^^xsd:integer .',
-  /* 4*/ '_:n3-2 rdf:rest rdf:nil .', // close @_:n3-2
-  /* 5*/ '_:n3-1 <p2> _:n3-2 .',
-  /* 6*/ '_:n3-0 rdf:rest _:n3-4 .',  // _:n3-0 ... ( [ <p5> 5 ] )
-  /* 7*/ '_:n3-4 rdf:first _:n3-5 .', // _:n3-0 ... ( [ <p5> 5 ] )
-  /* 8*/ '_:n3-5 <p5> "5"^^xsd:integer .',
-  /* 9*/ '_:n3-4 rdf:rest rdf:nil .', // close @_:n3-0
-  /*10*/ '<s1> <p1> _:n3-0 .',
-  /*11*/ '_:n3-6 <p7> "7"^^xsd:integer .',
-  /*12*/ '_:n3-6 <p8> "8"^^xsd:integer .',
-  /*13*/ '<s1> <p6> _:n3-6 .',
-  /*14*/ "<s9> <p9> 9 .",
-];
 const Ns = {
+  fhir: 'http://hl7.org/fhir/',
   rdf: "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
   xsd: "http://www.w3.org/2001/XMLSchema#",
 };
@@ -52,6 +25,7 @@ function transpose (graph) {
   const ret = {};
   const ignored = [];
   const lists = {};
+  const literals = {};
   let rootTerm = null;
   for (let target of graph) {
     const {subject: s, predicate: p, object: o} = target;
@@ -67,7 +41,7 @@ function transpose (graph) {
         {
           const appending = Object.keys(lists).find(key => lists[key].tail === sKey);
           if (!ret[keyFor(o)])
-            ret[keyFor(o)] = {}
+            ret[keyFor(o)] = o.termType === 'BlankNode' ? {} : jsonize(o)
           if (appending) {
             ret[appending].push(ret[keyFor(o)]);
           } else {
@@ -90,32 +64,45 @@ function transpose (graph) {
         }
         break;
       default:
-        if (!ret[sKey]) {
-          ret[sKey] = {}
-          if (s.termType === 'NamedNode') {
-            ret[sKey].id = s.value;
-          }
-        }
-        switch (o.termType) {
-        case 'BlankNode':
-          const oKey = keyFor(o);
-          ret[sKey][p.value] = ret[oKey];
-          delete ret[oKey];
+        const property = p.value.substring(Ns.fhir.length);
+        switch (property) {
+        case 'v':
+          literals[sKey] = o;
           break;
-        case 'NamedNode':
-          ret[sKey][p.value] = o.value;
-          break;
-        case 'Literal':
-          ret[sKey][p.value] = jsonize(o);
+        case 'link':
           break;
         default:
-          throw Error(`expected RdfJs triple or quad; got ${JSON.stringify(target)}`);
+          let o2 = o;
+          if (o.termType === 'BlankNode' && keyFor(o) in literals) {
+            o2 = literals[keyFor(o)];
+            delete literals[keyFor(o)];
+          }
+          if (!ret[sKey]) {
+            ret[sKey] = {}
+            // if (s.termType === 'NamedNode') {
+            //   ret[sKey].id = s.value;
+            // }
+          }
+          switch (o2.termType) {
+          case 'BlankNode':
+            const oKey = keyFor(o2);
+            ret[sKey][property] = ret[oKey];
+            delete ret[oKey];
+            break;
+          case 'NamedNode':
+            ret[sKey][property] = o2.value;
+            break;
+          case 'Literal':
+            ret[sKey][property] = jsonize(o2);
+            break;
+          default:
+            throw Error(`expected RdfJs triple or quad; got ${JSON.stringify(target)}`);
+          }
         }
       }
     }
   }
-  console.log(JSON.stringify(lists, null, 2))
-  return {resource: ret, ignored};
+  return {resource: ret[ROOT], ignored};
 }
 
 function keyFor (term) {
@@ -224,9 +211,56 @@ let s1_s9 = [
     }
   }
 ]
-
 blessTerms(s1_s9, {equals, toString: termToString});
-const tz = new N3.Parser({baseURI: 'http://a.example/some/base/file', format: 'text/turtle'})
+
+const NsTerm = "http://terminology.hl7.org/CodeSystem/";
+let turtle = `
+PREFIX fhir: <http://hl7.org/fhir/>
+PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+<smoker-1_smoking-2023-06-20>
+  fhir:id [ fhir:v "smoker-1_smoking-2023-06-20" ]; # _:n3-0
+  fhir:category ( # (_:n3-1, _:n3-7, _:n3-8)
+    [ fhir:coding  ( # _:n3-2 fhir:coding (_:n3-3)
+      [ fhir:system [ fhir:v "${NsTerm}observation-category"^^xsd:anyURI ]; # _:n3-4 fhir:system _:n3-5. _:n3-5 fhir:v "..."
+        fhir:code [fhir:v "social-history"] ]                                # _:n3-4 fhir:code _:n3-6. _:n3-6 fhir:v "social-history"
+    ) ]
+    "some literal"
+    [ fhir:text "boogers" ] # _:n3-6 fhir:text "boogers"                     # _:n3-9 fhir:text "..."
+  ) ;
+  fhir:subject [                                                             # fhir:subject _:n3-10
+    fhir:link <../Patient/smoker-1>;
+    fhir:reference [ fhir:v "Patient/smoker-1" ]                                  # _:n3-11 fhir:v "..."
+  ] .                                                                        # _:n3-7 fhir:link 7; fhir:reference 8
+<../Patient/smoker-1> a fhir:Patient .
+`;
+
+let tStrs = [
+  /* 0*/ '_:n3-0 fhir:v "smoker-1_smoking-2023-06-20" .',
+  /* 1*/ '<smoker-1_smoking-2023-06-20> fhir:id _:n3-0 .',
+  /* 2*/ '_:n3-1 rdf:first _:n3-2 .',
+  /* 3*/ '_:n3-3 rdf:first _:n3-4 .',
+  /* 4*/ '_:n3-5 fhir:v ".../observation-category"^^xsd:anyURI .',
+  /* 5*/ '_:n3-4 fhir:system _:n3-5 .',
+  /* 6*/ '_:n3-6 fhir:v "social-history" .',
+  /* 7*/ '_:n3-4 fhir:code _:n3-6 .',
+  /* 8*/ '_:n3-3 rdf:rest rdf:nil .',
+  /* 9*/ '_:n3-2 fhir:coding _:n3-3 .',
+  /*10*/ '_:n3-1 rdf:rest _:n3-7 .',
+  /*11*/ '_:n3-7 rdf:first "some literal" .',
+  /*12*/ '_:n3-7 rdf:rest _:n3-8 .',
+  /*13*/ '_:n3-8 rdf:first _:n3-9 .',
+  /*14*/ '_:n3-9 fhir:text "boogers" .',
+  /*15*/ '_:n3-8 rdf:rest rdf:nil .',
+  /*16*/ '<smoker-1_smoking-2023-06-20> fhir:category _:n3-1 .',
+  /*17*/ '_:n3-10 fhir:link <../abc> .',
+  /*18*/ '_:n3-11 fhir:v "Patient/smoker-1" .',
+  /*19*/ '_:n3-10 fhir:reference _:n3-11 .',
+  /*20*/ '<smoker-1_smoking-2023-06-20> fhir:subject _:n3-10 .',
+  /*21*/ '<../Patient/smoker-1> rdf:type fhir:Patient .'
+];
+
+
+const tz = new N3.Parser({baseIRI: 'http://a.example/Observation/', format: 'text/turtle'})
       .parse(turtle);
 blessTerms(tz, {toString: termToString});
 // console.log(tz.map(t => `${t.subject} ${t.predicate} ${t.object} .`))
